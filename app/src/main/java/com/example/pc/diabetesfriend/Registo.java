678f5dab83cee61127b0d66a10b49fb5fd096600
Mónica Francisco <monica.francisco@hotmail.com>
2015-11-16 17:39:14 +0000

    teste756

--- app/src/main/java/com/example/pc/diabetesfriend/Registo.java
+++ app/src/main/java/com/example/pc/diabetesfriend/Registo.java
@@ -1,13 +1,56 @@
 package com.example.pc.diabetesfriend;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class Registo extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+import android.widget.AdapterView.OnItemSelectedListener;
+
+public class Registo extends Activity implements OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registo);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        spinner.setOnItemSelectedListener(this);
+
+        List<String> categorias = new ArrayList<String>();
+        categorias.add("Masculino");
+        categorias.add("Feminino");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categorias);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    }
+
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
 }
