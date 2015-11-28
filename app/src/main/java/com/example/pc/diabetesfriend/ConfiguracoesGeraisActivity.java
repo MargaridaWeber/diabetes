317a3f03f17449d899f30aa0d68eb30042b3c18f
Ana Margarida <magizita@hotmail.com>
2015-11-28 02:14:17 +0000

    fragmentos

---
+++ app/src/main/java/com/example/pc/diabetesfriend/ConfiguracoesGeraisActivity.java
@@ -0,0 +1,44 @@
+package com.example.pc.diabetesfriend;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ConfiguracoesGeraisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuracoes_gerais);
+
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true); //setinha
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#e4e4e4")));
+        actionBar.setTitle(Html.fromHtml("<font color='#0060a2'>Configurações Gerais </font>")); //Cor do titulo
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true; default: return super.onOptionsItemSelected(item); }
+
+    }
+}
