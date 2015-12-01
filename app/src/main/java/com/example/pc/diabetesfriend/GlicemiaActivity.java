d7cdabade197718a83cccc7a843d80dbb8e407eb
Ana Margarida <magizita@hotmail.com>
2015-12-01 22:57:42 +0000

    hora

--- app/src/main/java/com/example/pc/diabetesfriend/GlicemiaActivity.java
+++ app/src/main/java/com/example/pc/diabetesfriend/GlicemiaActivity.java
@@ -1,6 +1,7 @@
 package com.example.pc.diabetesfriend;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.ActionBar;
@@ -12,6 +13,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -35,10 +38,26 @@ public class GlicemiaActivity extends AppCompatActivity {
 
         final EditText dataAtual = (EditText) findViewById(R.id.etdata);
         final EditText horaAtual = (EditText) findViewById(R.id.etHora);
+        final TextView iconData = (TextView) findViewById(R.id.icone_data);
+        final TextView iconHora = (TextView) findViewById(R.id.iconhora);
         dataAtual.setText(getDate());
         horaAtual.setText(getTime());
 
+
+
         dataAtual.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                dataPickerDialog();
+            }
+        });
+        iconData.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                dataPickerDialog();
+            }
+        });
+
+
+        iconHora.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 dataPickerDialog();
             }
@@ -46,13 +65,11 @@ public class GlicemiaActivity extends AppCompatActivity {
 
         horaAtual.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
-            }
-        });
-
+                timePickerDialog();
+                    }
+                });
 
         }
-
 
     public String getDate() {
 
@@ -65,6 +82,25 @@ public class GlicemiaActivity extends AppCompatActivity {
         DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
         Date date = new Date(); return dateFormat.format(date);
     }
+
+
+    private void timePickerDialog(){
+
+        Calendar mcurrentTime = Calendar.getInstance();
+        int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+        int minute = mcurrentTime.get(Calendar.MINUTE);
+        TimePickerDialog mTimePicker = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int selectedHour, int selectedMinute) {
+                EditText horaatual = (EditText) findViewById(R.id.etHora);
+                horaatual.setText(selectedHour+":"+ selectedMinute);
+            }
+        }, hour, minute, true);//Yes 24 hour time
+        mTimePicker.setTitle("Selecione a hora");
+        mTimePicker.show();
+
+    }
+
 
     private void dataPickerDialog(){
         final Calendar c = Calendar.getInstance();
--- app/src/main/java/com/example/pc/diabetesfriend/MainActivity.java
+++ app/src/main/java/com/example/pc/diabetesfriend/MainActivity.java
@@ -68,14 +68,6 @@ public class MainActivity extends AppCompatActivity
         });
 
 
-        Button btnAlarmes = (Button) findViewById(R.id.btnAlarmes);
-        btnAlarmes.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent alarmes = new Intent(getApplicationContext(), AlarmesActivity.class);
-                startActivity(alarmes);
-            }
-        });
-
         Button btnNutricao = (Button) findViewById(R.id.btnNutricao);
         btnNutricao.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
